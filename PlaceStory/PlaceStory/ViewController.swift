9dcf4c330055b49a5f4e2ead522a24806c2f30e5
최제환 <chlwpghks95@awasoft.co.kr>
2023-12-01 17:33:57 +0900

    :tada: begin Project

---
+++ PlaceStory/PlaceStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceStory
+//
+//  Created by 최제환 on 12/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlaceStory/PlaceStoryUITests/PlaceStoryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceStoryUITestsLaunchTests.swift
+//  PlaceStoryUITests
+//
+//  Created by 최제환 on 12/1/23.
+//
+
+import XCTest
+
+final class PlaceStoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
