8aeb0f4259e71a8350bc06fd735fb4d67de9f144
randychoi <jhc130833@naver.com>
2023-12-09 22:33:46 +0900

    - Add Platform's Sources(LocalStorage, Model, RepositoryImps)

--- PlaceStory/Platform/Package.swift
+++ PlaceStory/Platform/Package.swift
@@ -5,19 +5,44 @@ import PackageDescription
 
 let package = Package(
     name: "Platform",
+    platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Platform",
-            targets: ["Platform"]),
+            name: "LocalStorage",
+            targets: ["LocalStorage"]
+        ),
+        .library(
+            name: "Model",
+            targets: ["Model"]
+        ),
+        .library(
+            name: "RepositoryImps",
+            targets: ["RepositoryImps"]),
+    ],
+    dependencies: [
+      .package(path: "../Domain"),
+      .package(path: "../ProxyPackage")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Platform"),
-        .testTarget(
-            name: "PlatformTests",
-            dependencies: ["Platform"]),
+            name: "LocalStorage",
+            dependencies: [
+                "ProxyPackage",
+                .product(name: "Utils", package: "ProxyPackage")
+            ]
+        ),
+        .target(
+            name: "Model",
+            dependencies: [
+                "ProxyPackage"
+            ]
+        ),
+        .target(
+            name: "RepositoryImps",
+            dependencies: [
+                .product(name: "Repositories", package: "Domain"),
+                "LocalStorage"
+            ]
+        ),
     ]
 )
