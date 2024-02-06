71abb08df5fd1026c8a16d1018cd5af163b4445c
randychoi <jhc130833@naver.com>
2024-02-06 22:03:16 +0900

    :sparkles: Add Entity & Model
    - View에서 사용할 장소 기록 모델 PlaceRecord, Realm에 저장할 객체 PlaceRecordInfo 추가

---
+++ PlaceStory/Domain/Sources/Entities/PlaceRecord/PlaceRecord.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최제환 on 2/6/24.
+//
+
+import Foundation
+
+public struct PlaceRecord: Identifiable {
+    public let id: String
+    public let placeName: String
+    public let recordTitle: String
+    public let recordDescription: String
+    public let placeCategory: String
+    public let registerDate: Date
+    public let updateDate: Date
+    public let recordImages: [Data]?
+    
+    public init(
+        id: String,
+        placeName: String,
+        recordTitle: String,
+        recordDescription: String,
+        placeCategory: String,
+        registerDate: Date,
+        updateDate: Date,
+        recordImages: [Data]?
+    ) {
+        self.id = id
+        self.placeName = placeName
+        self.recordTitle = recordTitle
+        self.recordDescription = recordDescription
+        self.placeCategory = placeCategory
+        self.registerDate = registerDate
+        self.updateDate = updateDate
+        self.recordImages = recordImages
+    }
+}
---
+++ PlaceStory/Domain/Sources/UseCase/PlaceListUsecase.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최제환 on 2/6/24.
+//
+
+import Foundation
+
+public protocol PlaceListUsecase {
+    
+}
+
+public final class PlaceListUsecaseImp: PlaceListUsecase {
+    
+}
