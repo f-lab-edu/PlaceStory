8338dae8f28d52b12cbc47f84cf2e6913bff8a71
randychoi <jhc130833@naver.com>
2024-02-08 00:00:33 +0900

    :sparkles: Add PlaceListUsecase & PlaceListRepository
    - PlaceListUsecase 및 PlaceListRepository 추가
    - Domain의 Entity - PlaceRecord 과 Platform의 Model - PlaceRecordInfo 추가
    - 장소 기록에 대한 조회/저장/수정/삭제 등을 위해 RealmDatabase 리팩토링

--- PlaceStory/Domain/Sources/Repositories/PlaceListRepository.swift
+++ PlaceStory/Domain/Sources/Repositories/PlaceListRepository.swift
@@ -5,8 +5,14 @@
 //  Created by 최제환 on 2/6/24.
 //
 
+import Combine
+import Entities
 import Foundation
+import LocalStorage
 
 public protocol PlaceListRepository {
-    
+    func fetchPlaceRecordFrom(userId: String, placeName: String) -> AnyPublisher<[PlaceRecord], RealmDatabaseError>
+    func insert(placeRecord: PlaceRecord) -> Bool
+    func update(placeRecord: PlaceRecord) -> Bool
+    func delete(placeRecord: PlaceRecord) -> Bool
 }
--- PlaceStory/Platform/Sources/RepositoryImps/PlaceListRepositoryImp.swift
+++ PlaceStory/Platform/Sources/RepositoryImps/PlaceListRepositoryImp.swift
@@ -5,9 +5,117 @@
 //  Created by 최제환 on 2/6/24.
 //
 
+import Combine
+import Entities
 import Foundation
+import LocalStorage
+import Model
 import Repositories
+import RealmSwift
+import SecurityServices
+import Utils
 
-public final class PlaceListRepositoryImp: PlaceListRepository {
+public final class PlaceListRepositoryImp {
     
+    private let placeRecordSubject = CurrentValueSubject<[PlaceRecord], RealmDatabaseError>([])
+    private let database: RealmDatabaseImp
+    private let keychain: KeychainServiceImp
+    
+    public init(
+        database: RealmDatabaseImp,
+        keychain: KeychainServiceImp
+    ) {
+        self.database = database
+        self.keychain = keychain
+    }
+    
+    private func convertToPlaceRecordInfo(_ placeRecord: PlaceRecord) -> PlaceRecordInfo {
+        let placeRecordInfo = PlaceRecordInfo()
+        placeRecordInfo.id = database.convertObjectId(from: placeRecord.id)
+        placeRecordInfo.userId = placeRecord.userId
+        placeRecordInfo.placeName = placeRecord.placeName
+        placeRecordInfo.recordTitle = placeRecord.recordTitle
+        placeRecordInfo.recordDescription = placeRecord.recordDescription
+        placeRecordInfo.placeCategory = placeRecord.placeCategory
+        placeRecordInfo.registerDate = placeRecord.registerDate
+        placeRecordInfo.updateDate = placeRecord.updateDate
+        if let recordImages = placeRecord.recordImages {
+            placeRecordInfo.recordImages.append(objectsIn: recordImages)
+        }
+        
+        return placeRecordInfo
+    }
+    
+    private func convertObjectToDictionary(from object: Object) -> [String: Any] {
+        let mirror = Mirror(reflecting: object)
+        
+        var dict = [String: Any]()
+        for child in mirror.children {
+            if let key = child.label {
+                dict[key] = child.value
+            }
+        }
+        
+        return dict
+    }
+}
+
+// MARK: - PlaceListRepository
+
+extension PlaceListRepositoryImp: PlaceListRepository {
+    public func fetchPlaceRecordFrom(userId: String, placeName: String) -> AnyPublisher<[PlaceRecord], RealmDatabaseError> {
+        let result = database.read(PlaceRecordInfo.self, userId: userId, placeName: placeName)
+        
+        switch result {
+        case .success(let placeRecordInfos):
+            let placeRecords = placeRecordInfos.map { $0.toDomain() }
+            placeRecordSubject.send(placeRecords)
+            
+        case .failure(let realmDatabaseError):
+            placeRecordSubject.send(completion: .failure(realmDatabaseError))
+        }
+        
+        return placeRecordSubject.eraseToAnyPublisher()
+    }
+    
+    public func insert(placeRecord: PlaceRecord) -> Bool {
+        let placeRecordInfo = convertToPlaceRecordInfo(placeRecord)
+        let result = database.create(placeRecordInfo)
+        
+        switch result {
+        case .success(let success):
+            return success
+            
+        case .failure:
+            return false
+        }
+    }
+    
+    public func update(placeRecord: PlaceRecord) -> Bool {
+        let placeRecordInfo = convertToPlaceRecordInfo(placeRecord)
+        let placeReCordInfoDictionary = convertObjectToDictionary(from: placeRecordInfo)
+        
+        let result = database.update(PlaceRecordInfo.self, forKey: placeRecordInfo.id, with: placeReCordInfoDictionary)
+        
+        switch result {
+        case .success(let success):
+            return success
+            
+        case .failure:
+            return false
+        }
+    }
+    
+    public func delete(placeRecord: PlaceRecord) -> Bool {
+        let placeRecordInfo = convertToPlaceRecordInfo(placeRecord)
+        let result = database.delete(placeRecordInfo)
+        
+        switch result {
+        case .success(let success):
+            return success
+            
+        case .failure:
+            return false
+        }
+    }
 }
