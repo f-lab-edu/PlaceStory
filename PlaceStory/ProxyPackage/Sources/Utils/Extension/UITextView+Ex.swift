8a68a181fd82cce4b7d1b0cbd97a11ca5e00730f
randychoi <jhc130833@naver.com>
2024-03-12 00:13:50 +0900

    :lipstick: 장소 기록 화면 - 제목/내용 추가

---
+++ PlaceStory/ProxyPackage/Sources/Utils/Extension/UITextView+Ex.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최제환 on 3/12/24.
+//
+
+import UIKit
+
+extension UITextView {
+    public func addDoneButtonOnToolbar() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc private func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
